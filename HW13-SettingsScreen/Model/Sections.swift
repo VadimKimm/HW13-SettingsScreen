1351ff5f51aa95013b552691f71fda158df0201f
Vadim <vadim_kimm@icloud.com>
2022-06-05 18:54:01 +0300

    Modified enum CellType

--- HW13-SettingsScreen/Model/Sections.swift
+++ HW13-SettingsScreen/Model/Sections.swift
@@ -13,11 +13,15 @@ struct Sections {
 
 enum CellType {
     case simpleCell(SimpleCellModel)
+    case extendedInfoCell(ExtendedInfoCellModel)
 
     func configureCell(cell: UITableViewCell, model: CellType) {
         switch self {
         case .simpleCell(let model):
             let cell = cell as! SimpleTableViewCell
+            cell.configure(with: model)
+        case .extendedInfoCell(let model):
+            let cell = cell as! ExtendedInfoTableViewCell
             cell.configure(with: model)
         }
     }
