0f5f8a4dba6701cfd85ee262fe5f6abb696e2583
Vadim <vadim_kimm@icloud.com>
2022-06-05 16:57:03 +0300

    Initial Commit

---
+++ HW13-SettingsScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13-SettingsScreen
+//
+//  Created by Vadim Kim on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
