b0da9448b49e3279297197d970f78e3a12baf12a
Vadim <vadim_kimm@icloud.com>
2022-06-05 17:57:07 +0300

    Created struct Sections and enum CellType

---
+++ HW13-SettingsScreen/Model/Sections.swift
@@ -0,0 +1,16 @@
+//
+//  Sections.swift
+//  HW13-SettingsScreen
+//
+//  Created by Vadim Kim on 05.06.2022.
+//
+
+import UIKit
+
+struct Sections {
+    let items: [CellType]
+}
+
+enum CellType {
+    case simpleCell(SimpleCellModel)
+}
